7a3c9a1f6b701f515acbb00c295554397d1c8d67
samajeste1 <9995@holbertonstudents.com>
2025-09-05 22:20:07 +0200

    m

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
+import sys
+
 if __name__ == "__main__":
-    import sys
-
-    args = sys.argv[1:]
-    count = len(args)
-
-    if count == 0:
+    if len(sys.argv) == 1:
         print("0 arguments.")
-    elif count == 1:
+    elif len(sys.argv) == 2:
         print("1 argument:")
+        print(f"1: {sys.argv[1]}")
     else:
-        print("{} arguments:".format(count))
-
-    for i in range(count):
-        print("{}: {}".format(i + 1, args[i]))
+        print(f"{len(sys.argv[1:])} arguments:")
+        for x in range(len(sys.argv[1:])):
+            print(f"{x + 1}: {sys.argv[x + 1]}")
+            
--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import sys
+import sys
 
-    total = 0
-    for arg in sys.argv[1:]:
-        total += int(arg)
-    print(total)
+if __name__ == "__main__":
+    add = 0
+    for x in sys.argv[1:]:
+        add += int(x)
+    print(add)
+    
