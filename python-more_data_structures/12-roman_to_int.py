722c058fc7108a7b30e9b17fcfd2c67a73e65e4c
samajeste1 <9995@holbertonstudents.com>
2025-10-09 04:48:52 +0200

    m

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str):
+        return 0
+    roman_map = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+                 'C': 100, 'D': 500, 'M': 1000}
+    total = 0
+    prev = 0
+    for c in reversed(roman_string):
+        value = roman_map.get(c, 0)
+        if value < prev:
+            total -= value
+        else:
+            total += value
+        prev = value
+    return total
